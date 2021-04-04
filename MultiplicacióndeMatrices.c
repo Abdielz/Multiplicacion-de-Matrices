060f1a0b290b8e2585d071f60325a76c56e52fa0
Abdielz <80777678+Abdielz@users.noreply.github.com>
2021-04-04 17:03:33 -0500

    Version 3, Multiplicacion de Matrices exitosa

--- MultiplicacióndeMatrices.c
+++ MultiplicacióndeMatrices.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int FA,CA,FB,CB,Co=0,i,j,k;
+int FA,CA,FB,CB,FC,CC,Co=0,C1=0,C2=0,i,j,k;
 
 
 int main()
@@ -34,7 +34,48 @@ Matriz1=fopen("Matriz1.txt","r");
             };
         };
    };
-fclose(Matriz1);   
+fclose(Matriz1); 
+
+FILE *Matriz2;
+Matriz2=fopen("Matriz2.txt","r");
+   while (C1<FB)
+   {
+       printf("\nMatriz 2:");
+       for (i = 0; i < FB; ++i)
+       {
+        printf("\n");
+            for (j = 0; j < CB; ++j)
+            {
+            fscanf(Matriz2,"%d", &B[i][j]);
+            printf("%d\t", B[i][j]);
+            C1=C1+1;
+            };
+        };
+   };
+fclose(Matriz2); 
+
+for (i = 0; i < FA; ++i)
+{
+    for (j = 0; j < CB; ++j)
+    {
+        C[i][j]=0;
+        for (k = 0; k < CA; ++k)
+        {
+            C[i][j]+=A[i][k]*B[k][j];
+        }
+    }   
+}
+
+    printf("\nMatriz Resultante\n");
+    for (i = 0; i < FB; i++) 
+    {
+        for (j = 0; j < CB; j++) 
+        {
+            printf("%d\t ", C[i][j]);
+        }
+        printf("\n");
+    }
+
 }
 else
 {
